fc47d3d05d1d49a3cbd82a693259262b6294ca04
James Hobin <hobinjk@mit.edu>
2016-06-28 11:03:28 -0400

    Load editor files in parallel; Resize iframe based on content

--- libraries/webInterface/js/editor.js
+++ libraries/webInterface/js/editor.js
@@ -1,43 +1,67 @@
 require.config({paths: {'vs': '/libraries/monaco-editor/min/vs'}});
 var container = document.getElementById('editor-container');
+var previewFrame = document.getElementById('preview-frame');
 
 var xhr = new XMLHttpRequest();
 
 var saveTimeout = null;
+var editor = null;
 
 xhr.open('GET', path);
 
 xhr.onload = function() {
-    require(['vs/editor/editor.main'], function() {
-        var editor = monaco.editor.create(container, {
-            value: xhr.responseText,
-            language: 'html'
-        });
-
-        editor.addListener('contentChanged', updatePreview);
-        var previewFrame = document.getElementById('preview-frame');
-
-        function updatePreview() {
-            var value = editor.getValue();
-            if (saveTimeout) {
-                window.clearTimeout(saveTimeout);
-            }
-            saveTimeout = window.setTimeout(save, 250);
-        }
-
-        function save() {
-            var saveXhr = new XMLHttpRequest();
-            saveXhr.open('PUT', document.location);
-            saveXhr.onload = function() {
-                previewFrame.src = previewFrame.src;
-            };
-            var formData = new FormData();
-            formData.append('content', editor.getValue());
-            saveXhr.setRequestHeader('Content-Type', 'application/json');
-            saveXhr.send(JSON.stringify({content: editor.getValue()}));
-            saveTimeout = null;
-        }
-    });
+    if (editor) {
+        editor.setValue(xhr.responseText);
+    }
 };
+
+require(['vs/editor/editor.main'], function() {
+    var value = '';
+    if (xhr.readyState === 4) { // if xhr is done
+        value = xhr.responseText;
+    }
+
+    editor = monaco.editor.create(container, {
+        value: value,
+        language: 'html'
+    });
+
+    editor.addListener('contentChanged', updatePreview);
+
+    function updatePreview() {
+        var value = editor.getValue();
+        if (saveTimeout) {
+            window.clearTimeout(saveTimeout);
+        }
+        saveTimeout = window.setTimeout(save, 250);
+    }
+
+    function save() {
+        var saveXhr = new XMLHttpRequest();
+        saveXhr.open('PUT', document.location);
+        saveXhr.onload = function() {
+            previewFrame.src = previewFrame.src;
+        };
+        var formData = new FormData();
+        formData.append('content', editor.getValue());
+        saveXhr.setRequestHeader('Content-Type', 'application/json');
+        saveXhr.send(JSON.stringify({content: editor.getValue()}));
+        saveTimeout = null;
+    }
+});
+
+previewFrame.addEventListener('load', function() {
+    var data = {
+        pos: 'fake',
+        obj: 'also fake'
+    };
+    previewFrame.contentWindow.postMessage(JSON.stringify(data), '*');
+});
+
+window.addEventListener('message', function(e) {
+    var data = JSON.parse(e.data);
+    previewFrame.width = data.width;
+    previewFrame.height = data.height;
+});
 
 xhr.send();
