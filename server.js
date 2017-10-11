317c788150bdf1f70d62cd830873bf84f3e3d2c4
Ben Reynolds <breynolds@ptc.com>
2017-10-11 19:48:26 -0400

    Added connection listener to API so that interfaces can receive notifications when their nodes are connected to a link

--- server.js
+++ server.js
@@ -1563,7 +1563,37 @@ function objectWebServer() {
                 utilities.writeObjectToFile(objects, req.params[0], __dirname);
                 // write the object state to the permanent storage.
                 socketUpdater();
-				// call an action that asks all devices to reload their links, once the links are changed.
+
+                // notify subscribed interfaces that a new link was created // TODO: make sure this is the right place for this
+                // hardwareAPI.connectCall(msgContent.object, msgContent.node, true);
+                console.log("req.params[0] is " + req.params[0]);
+                console.log("req.params[1] is " + req.params[1]);
+
+                var linkObjectA = thisObject["objectA"];
+                var linkObjectB = thisObject["objectB"];
+                var linkNodeA = thisObject["nodeA"];
+                var linkNodeB = thisObject["nodeB"];
+                var objectAName = objects[linkObjectA].name;
+                var objectBName = objects[linkObjectB].name;
+                var nodeAName = objects[linkObjectA].nodes[linkNodeA].name;
+                var nodeBName = objects[linkObjectB].nodes[linkNodeB].name;
+
+                var linkAddedData = {
+                    added: true,
+                    idObjectA: linkObjectA,
+                    idObjectB: linkObjectB,
+                    idNodeA: linkNodeA,
+                    idNodeB: linkNodeB,
+                    nameObjectA: objectAName,
+                    nameObjectB: objectBName,
+                    nameNodeA: nodeAName,
+                    nameNodeB: nodeBName
+                };
+
+                hardwareAPI.connectCall(linkObjectA, linkNodeA, linkAddedData);
+                hardwareAPI.connectCall(linkObjectB, linkNodeB, linkAddedData);
+
+                // call an action that asks all devices to reload their links, once the links are changed.
 				actionSender({reloadLink: {object: req.params[0]}, lastEditor: req.body.lastEditor});
             } else {
                 updateStatus = "found endless Loop";
