7e4491707cd04a1e319397823936f65c901c1cbf
Ben Reynolds <breynolds@ptc.com>
2018-02-20 14:00:36 -0500

    fixes css of frames so to disable touch-callout and user-select

--- libraries/objectDefaultFiles/object.js
+++ libraries/objectDefaultFiles/object.js
@@ -70,7 +70,7 @@ var realityObject = {
 
 // adding css styles nessasary for acurate 3D transformations.
 realityObject.style.type = 'text/css';
-realityObject.style.innerHTML = 'body, html{ height: 100%; margin:0; padding:0;}';
+realityObject.style.innerHTML = '* {-webkit-user-select: none; -webkit-touch-callout: none;} body, html{ height: 100%; margin:0; padding:0;}';
 document.getElementsByTagName('head')[0].appendChild(realityObject.style);
 
 // Load socket.io.js synchronous so that it is available by the time the rest of the code is executed.
