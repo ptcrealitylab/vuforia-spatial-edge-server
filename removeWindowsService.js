fa7f148514c9fc6b8138d4f8a3b126631ce8a483
valentinptc <vheun@ptc.com>
2018-06-04 14:03:42 -0400

    events

---
+++ removeWindowsService.js
@@ -0,0 +1,17 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+    name:'Reality Server',
+    description: 'Real-Time service for Reality Editor',
+    script: require('path').join(__dirname,'runServerForever.js')
+});
+
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall',function(){
+    console.log('Uninstall complete.');
+    console.log('The service exists: ',svc.exists);
+});
+
+// Uninstall the service.
+svc.uninstall();
--- windowsService.js
+++ windowsService.js
@@ -1,10 +1,22 @@
 var Service = require('node-windows').Service;
 
+var EventLogger = require('node-windows').EventLogger;
+
+var log = new EventLogger('Reality Server');
+
+log.info('Basic information.');
+log.warn('Watch out!');
+log.error('Something went wrong.');
+
 // Create a new service object
 var svc = new Service({
     name:'Reality Server',
     description: 'Real-Time service for Reality Editor',
-    script: require('path').join(__dirname,'runServerForever.js')
+    script: require('path').join(__dirname,'runServerForever.js'),
+    env: {
+        name: "HOME",
+        value: process.env["USERPROFILE"] // service is now able to access the user who created its' home directory
+    }
 });
 
 // Listen for the "install" event, which indicates the
