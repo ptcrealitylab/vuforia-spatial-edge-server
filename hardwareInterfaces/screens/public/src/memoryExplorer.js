b46fea3fab4664a89dc1d6268d4ff01c033928fa
Ben Reynolds <breynolds@ptc.com>
2019-05-06 15:40:05 -0400

    starts to split up memoryExplorer monolithic file into several modules... memoryLinkRenderer, memoryNodeRenderer, and memoryExplorer

--- hardwareInterfaces/screens/public/src/memoryExplorer.js
+++ hardwareInterfaces/screens/public/src/memoryExplorer.js
@@ -1,9 +1,36 @@
 createNameSpace("realityEditor.memoryExplorer");
 
+/**
+ * @fileOverview realityEditor.memoryExplorer.js
+ * Provides the ability to show and hide their memories using the objectDiscovery menu in the form of memoryFrames
+ * Provides functions to interface with memoryFrames and their content in customized ways (e.g. inject guiState)
+ */
+
 (function(exports) {
 
-    function initFeature() {
+    var guiState;
 
+    function initFeature() {
+        realityEditor.modeToggle.addGuiStateListener(function(newGuiState) {
+            guiState = newGuiState;
+            injectGuiStateToMemories(newGuiState);
+        });
+    }
+
+    function injectGuiStateToMemories(newGuiState) {
+        var allMemoryFrames = Object.keys(frames).map(function(frameKey) {
+            return frames[frameKey];
+        }).filter(function(frame) {
+            return frame.visualization === 'screen' && frame.src === 'memoryFrame';
+        });
+
+        allMemoryFrames.forEach(function(memoryFrame) {
+            var iframe = document.getElementById('iframe' + memoryFrame.uuid);
+            iframe.contentWindow.postMessage(JSON.stringify({
+                guiState: newGuiState,
+                platform: 'desktop'
+            }), '*');
+        });
     }
 
     exports.initFeature = initFeature;
