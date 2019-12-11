e9daf11aad49790d23c0e7b18971e95b7f2d852b
Ben Reynolds <breynolds@ptc.com>
2019-12-11 12:31:10 -0500

    updates object version to 3.2.0 and includes an ar.z property in addition to x and y, for completeness, but ensuring backwards compatibility for older objects without this property. Also finishes first version of human pose objects. Moves constructor to external module (libraries/HumanObject.js) and finishes/cleans up the socket.on(/update/humanPoses) handler to create/delete HumanObjects and update their frame positions based on skeleton joint positions

--- server.js
+++ server.js
@@ -202,6 +202,15 @@ var webFrontend = require(__dirname + '/libraries/webFrontend');
 // Definition for a simple API for hardware interfaces talking to the server.
 // This is used for the interfaces defined in the hardwareAPI folder.
 var hardwareAPI;
+// This file hosts the constructor and class methods for human pose objects (generated from kinect skeleton data)
+var { HumanObject, humanObjectDependencies } = require(__dirname + '/libraries/HumanObject');
+humanObjectDependencies.inject({
+    ips: ips,
+    version: version,
+    protocol: protocol,
+    Frame: Frame,
+    Node: Node
+});
 
 if (isMobile) {
     hardwareAPI = require('./libraries/mobile/hardwareInterfaces');
@@ -270,145 +279,6 @@ function Objects() {
         height: 0.3
     }
 
-}
-
-// matches the entries of the Azure Kinect Body Tracking SDK
-// k4abt_joint_id_t (https://microsoft.github.io/Azure-Kinect-Body-Tracking/release/0.9.x/group__btenums.html#ga5fe6fa921525a37dec7175c91c473781)
-var POSE_JOINTS = Object.freeze({
-    JOINT_PELVIS: 0,
-    JOINT_SPINE_NAVAL: 1,
-    JOINT_SPINE_CHEST: 2,
-    JOINT_NECK: 3,
-    JOINT_CLAVICLE_LEFT: 4,
-    JOINT_SHOULDER_LEFT: 5,
-    JOINT_ELBOW_LEFT: 6,
-    JOINT_WRIST_LEFT: 7,
-    JOINT_HAND_LEFT: 8,
-    JOINT_HANDTIP_LEFT: 9,
-    JOINT_THUMB_LEFT: 10,
-    JOINT_CLAVICLE_RIGHT: 11,
-    JOINT_SHOULDER_RIGHT: 12,
-    JOINT_ELBOW_RIGHT: 13,
-    JOINT_WRIST_RIGHT: 14,
-    JOINT_HAND_RIGHT: 15,
-    JOINT_HANDTIP_RIGHT: 16,
-    JOINT_THUMB_RIGHT: 17,
-    JOINT_HIP_LEFT: 18,
-    JOINT_KNEE_LEFT: 19,
-    JOINT_ANKLE_LEFT: 20,
-    JOINT_FOOT_LEFT: 21,
-    JOINT_HIP_RIGHT: 22,
-    JOINT_KNEE_RIGHT: 23,
-    JOINT_ANKLE_RIGHT: 24,
-    JOINT_FOOT_RIGHT: 25,
-    JOINT_HEAD: 26,
-    JOINT_NOSE: 27,
-    JOINT_EYE_LEFT: 28,
-    JOINT_EAR_LEFT: 29,
-    JOINT_EYE_RIGHT: 30,
-    JOINT_EAR_RIGHT: 31
-});
-
-function HumanObject(bodyId) {
-    // The ID for the object will be broadcasted along with the IP. It consists of the name with a 12 letter UUID added.
-    this.objectId = getHumanObjectID(bodyId); //name + utilities.uuidTime();
-    // The name for the object used for interfaces.
-    this.name = getHumanObjectName(bodyId); //bodyId; //name;
-    // The IP address for the object is relevant to point the Reality Editor to the right server.
-    // It will be used for the UDP broadcasts.
-    this.ip = ips.interfaces[ips.activeInterface];
-    // The version number of the Object.
-    this.version = version;
-    this.deactivated = false;
-    this.protocol = protocol;
-    // The (t)arget (C)eck(S)um is a sum of the checksum values for the target files.
-    this.tcs = null;
-    // Intended future use is to keep a memory of the last matrix transformation when interacted.
-    // This data can be used for interacting with objects for when they are not visible.
-    this.memory = {};
-    this.memoryCameraMatrix = {};
-    this.memoryProjectionMatrix = {};
-    // Store the frames. These embed content positioned relative to the object
-    this.frames = this.createPoseFrames();
-    // keep a memory of the last commit state of the frames.
-    this.framesHistory = {};
-    // which visualization mode it should use right now ("ar" or "screen")
-    this.visualization = "ar";
-    this.zone = "";
-    // taken from target.xml. necessary to make the screens work correctly.
-    this.targetSize = {
-        width: 0.3, // default size should always be overridden, but exists in case xml doesn't contain size
-        height: 0.3
-    };
-    this.isHumanPose = true;
-}
-
-HumanObject.prototype.createPoseFrames = function() {
-    var frames = {};
-    Object.keys(POSE_JOINTS).forEach(function(jointName) {
-        frames[ this.getFrameKey(jointName) ] = this.createFrame(jointName);
-    }.bind(this));
-    return frames;
-};
-
-HumanObject.prototype.getFrameKey = function(jointName) {
-    return this.objectId + jointName;
-};
-
-HumanObject.prototype.createFrame = function(jointName) {
-    var newFrame = new Frame();
-    newFrame.objectId = this.objectId;
-    newFrame.uuid = this.getFrameKey(jointName); //this.objectId + frameName;// + utilities.uuidTime();
-    newFrame.name = jointName;
-    newFrame.ar.scale = 0.25;
-    return newFrame;
-};
-
-HumanObject.prototype.updateJointPositions = function(joints) {
-    
-    // todo: convert joint position from meters to mm?
-    var scale = 100;
-
-    var objPos = {
-        x: joints[0].x * scale, // right now uses the pelvis, but could change to any other joint
-        y: joints[0].y * scale,
-        z: joints[0].z * scale
-    };
-
-    this.matrix = [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 0, 1,
-        objPos.x, objPos.y, 0, 1
-        // objPos.x, objPos.y, 0, 1
-    ];
-    
-    // update the position of each frame based on the poseInfo
-    joints.forEach(function(position, i) {
-        var jointName = Object.keys(POSE_JOINTS)[i];
-        var frame = this.frames[this.getFrameKey(jointName)];
-        if (frame) {
-            frame.ar.matrix = [
-                1, 0, 0, 0,
-                0, 1, 0, 0,
-                0, 0, 0, 1,
-                objPos.x - position.x * scale, objPos.y - position.y * scale, 0, 1
-                // objPos.x - position.x * scale, objPos.y - position.y * scale, 0, 1
-            ];
-            // frame.ar.x = objPos.x - position.x;
-            // frame.ar.y = objPos.y - position.y;
-            // frame.ar.z = objPos.z - position.z;
-        }
-    }.bind(this));
-};
-
-// converts bodyID from Kinect into a UUID for an object
-function getHumanObjectID(bodyId) {
-    return 'humanObject' + bodyId;
-}
-
-function getHumanObjectName(bodyId) {
-    return 'human' + bodyId;
 }
 
 function Frame() {
@@ -4905,59 +4775,76 @@ function socketServer() {
             if (typeof msg === 'string') {
                 msgContent = JSON.parse(msg);
             }
+            if (!msgContent) { return; }
             
-            forEachHumanObject(function(thisObject) {
+            // if no poses changed this frame, don't clog the network with sending the same information
+            var didAnythingChange = false;
+            forEachHumanObject(function(objectKey, thisObject) {
                 thisObject.wasUpdated = false;
             });
             
             msgContent.forEach(function(poseInfo) {
-                if (!doesObjectExist( getHumanObjectID(poseInfo.id) )) {
-                    // create an object
-                    objects[getHumanObjectID(poseInfo.id)] = new HumanObject(poseInfo.id);
-                    
-                    console.log('created human pose object');
-
+                var objectId = HumanObject.getObjectId(poseInfo.id);
+                var thisObject = objects[objectId];
+                if (!doesObjectExist(objectId)) {
+                    // create an object if needed
+                    objects[objectId] = new HumanObject(poseInfo.id);
                     // advertise to editors
-                    objectBeatSender(beatPort, getHumanObjectID(poseInfo.id), objects[getHumanObjectID(poseInfo.id)].ip, true);
-
-                    // todo: writeObjectToFile? needs to create directory in realityobjects etc
-                    // ^ might not actually be needed, why would human object need to persist?
+                    objectBeatSender(beatPort, objectId, thisObject.ip, true);
+                    // currently doesn't writeObjectToFile, because I've found no need for human objects to persist 
                 }
-                var thisObject = objects[getHumanObjectID(poseInfo.id)];
                 // update the position of each frame based on the poseInfo
                 thisObject.updateJointPositions(poseInfo.joints);
-                thisObject.wasUpdated = true;
-                
-                console.log(thisObject.name);
+                thisObject.wasUpdated = true; // flags objects to be deleted if they are not updated
+                didAnythingChange = true;
             });
             
             // check if any Human Objects were not contained in msgContent, and delete them
             forEachHumanObject(function(objectKey, thisObject) {
                 if (!thisObject.wasUpdated) {
-                    console.log('delete human pose object', thisObject);
+                    console.log('delete human pose object', objectKey);
+                    didAnythingChange = true;
                     delete objects[objectKey];
                     // todo: delete folder recursive if necessary?
                     // ^ might not actually be needed, why would human object need to persist?
                 }
             });
 
-            // send updated objects / positions to editors
+            if (!didAnythingChange) { return; }
+            
+            // send updated objects / positions to all known editors. right now piggybacks on sockets opened for other realtime communications.
             for (var socketId in realityEditorObjectMatrixSocketArray) {
-                // if (msgContent.hasOwnProperty('editorId') && realityEditorUpdateSocketArray[socketId] && msgContent.editorId === realityEditorUpdateSocketArray[socketId].editorId) {
-                //     continue; // don't send updates to the editor that triggered it
-                // }
-
                 var thisSocket = io.sockets.connected[socketId];
                 if (thisSocket) {
-                    // console.log('update human poses for editor (' + realityEditorUpdateSocketArray[socketId].editorId + ')');
-
+                    // sends an array of objectIds for the visible poses in this timestep
                     var visibleHumanObjects = Object.keys(objects).filter(function(objectKey) {
                         return objects[objectKey].isHumanPose;
                     });
-                    
                     var updateResponse = {
                         visibleHumanObjects: visibleHumanObjects
                     };
+                    // also sends all object JSON data for each visible pose object
+                    var objectData = {};
+                    visibleHumanObjects.forEach(function(objectKey) {
+                        objectData[objectKey] = objects[objectKey];
+                    });
+                    updateResponse.objectData = objectData;
+
+                    // TODO: we should only need to send object matrix, and each frame.ar (x,y,matrix) for each pose object
+                    // this doesn't work right now because we also need the full JSON to instantly create the object on the client for a newly detected pose
+                    // but there might be a way in the future to only send full data for objects that are newly detected
+                    // to reduce the bandwidth used by constantly sending object/frame/pose information to all clients
+                    // var compressedObjectData = {};
+                    // visibleHumanObjects.forEach(function(objectKey) {
+                    //     compressedObjectData[objectKey] = {
+                    //         ip: objects[objectKey].ip
+                    //         matrix: objects[objectKey].matrix
+                    //     };
+                    //     for (var frameKey in objects[objectKey].frames) {
+                    //         compressedObjectData[frameKey] = objects[objectKey].frames[frameKey].ar;
+                    //     }
+                    // });
+                    // updateResponse.compressedObjectData = compressedObjectData;
 
                     thisSocket.emit('/update/humanPoses', JSON.stringify(updateResponse));
                 }
