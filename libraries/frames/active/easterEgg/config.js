016341f72c518625ff6d75ded47a8b80a393937f
Ben Reynolds <breynolds@ptc.com>
2019-12-27 13:49:23 -0500

    fixes easterEgg and pushMe frames

--- libraries/frames/active/easterEgg/config.js
+++ libraries/frames/active/easterEgg/config.js
@@ -3,8 +3,8 @@ exports.properties = {
     showInPocket: true,
     tags: ['entertainment'],
     nodes: [
-        { name: 'x', type: 'node', x: -150, y: 0 },
+        { name: 'x', type: 'node', x: -75, y: 0 },
         { name: 'y', type: 'node', x: 0, y: 0 },
-        { name: 'reset', type: 'node', x: 150, y: 0 }
+        { name: 'reset', type: 'node', x: 75, y: 0 }
     ]
 };
