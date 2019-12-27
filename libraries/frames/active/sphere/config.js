ee55c2c7e78c86ca39dc89df69bce2958113a050
Ben Reynolds <breynolds@ptc.com>
2019-12-27 14:24:12 -0500

    adds nodes to sphere frame to adjust color

--- libraries/frames/active/sphere/config.js
+++ libraries/frames/active/sphere/config.js
@@ -3,8 +3,8 @@ exports.properties = {
     showInPocket: true,
     tags: ['3d', 'output'],
     nodes: [
-        { name: 'hue', type: 'node', x: -50, y: -50 },
-        { name: 'saturation', type: 'node', x: 0, y: 0 },
-        { name: 'lightness', type: 'node', x: 50, y: 50 }
+        { name: 'hue', type: 'node', x: -50, y: -50, defaultValue: 0 },
+        { name: 'saturation', type: 'node', x: 0, y: 0, defaultValue: 0.75 },
+        { name: 'lightness', type: 'node', x: 50, y: 50, defaultValue: 0.6 }
     ]
 };
