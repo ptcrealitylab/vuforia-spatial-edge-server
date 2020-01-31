7c7be7aa782685e198d5e22625cd10c626010f3f
James Hobin <jhobin@ptc.com>
2020-01-31 15:37:35 -0500

    Add eslint

---
+++ hardwareInterfaces/screens/public/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'browser': true
+    },
+    'extends': '../../../.eslintrc.js',
+    'globals': {
+        'realityEditor': 'readonly',
+        'createNameSpace': 'readonly',
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+    }
+};
+
+
---
+++ libraries/webInterface/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'browser': true
+    },
+    'extends': '../../.eslintrc.js',
+    'globals': {
+        'realityServer': 'readonly',
+        'Dropzone': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+    }
+};
+
