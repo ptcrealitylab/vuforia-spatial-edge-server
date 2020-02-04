6a91b5b2adab4b6fc429032b5efa33d7e641baaf
James Hobin <jhobin@ptc.com>
2020-02-04 13:47:46 -0500

    Improve logger to be a better drop-in replacement for console.log

--- logger.js
+++ logger.js
@@ -1,28 +1,58 @@
 const winston = require('winston');
+const os = require('os');
+const { createLogger, format, transports } = require('winston');
+const { inspect } = require('util');
+const hasAnsi = require('has-ansi');
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        winston.format.errors({ stack: true }),
-        winston.format.splat(),
-        winston.format.json()
-    ),
-    transports: [
-        new winston.transports.File({ filename: 'quick-start-error.log', level: 'error' }),
-    ],
+const isMobile = os.platform() === 'android' || os.platform() === 'ios';
+
+// From https://github.com/winstonjs/winston/issues/1427#issuecomment-535297716
+function isPrimitive(val) {
+    return val === null || (typeof val !== 'object' && typeof val !== 'function');
+}
+function formatWithInspect(val, colorize) {
+    const prefix = isPrimitive(val) ? '' : '\n';
+    const shouldFormat = typeof val !== 'string' || !hasAnsi(val);
+
+    return prefix + (shouldFormat ? inspect(val, { depth: null, colors: colorize }) : val);
+}
+function fancyPrintf(colorize) {
+    return function(info) {
+        const msg = formatWithInspect(info.message, colorize);
+        const splatArgs = info[Symbol.for('splat')] || [];
+        const rest = splatArgs.map(data => formatWithInspect(data)).join(' ');
+
+        return `${info.timestamp} - ${info.level}: ${msg} ${rest}`;
+    };
+}
+
+const monochromeFormat = format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }),
+    format.printf(fancyPrintf(!isMobile))
+);
+
+const colorizedFormat = format.combine(
+    format.timestamp(),
+    format.errors({ stack: true }),
+    format.colorize(),
+    format.printf(fancyPrintf(!isMobile))
+);
+
+const logger = createLogger({
+    level: 'debug',
+    format: isMobile ? monochromeFormat : colorizedFormat,
+    transports: [new transports.Console()]
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        level: 'debug',
-        format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
-        ),
-    }))
+if (process.env.NODE_ENV === 'production') {
+    logger.level = 'info';
+    if (!isMobile) {
+        logger.add(new winston.transports.File({
+            filename: 'error.log',
+            level: 'error'
+        }));
+    }
 }
 
 module.exports = logger;
