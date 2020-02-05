c730c9c5d36c8376cef1f1f04482e69e102ec05a
James Hobin <jhobin@ptc.com>
2020-02-05 16:16:13 -0500

    Delete modules consistent with miniServer branch

--- scripts/mobile-cleanup.sh
+++ scripts/mobile-cleanup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -ex
-# rm -fr .git # this would be good but prevents undoing this
-npm remove --no-save monaco-editor sharp
+
+# this would be good but prevents undoing this
+# rm -fr .git
+rm -r hardwareInterfaces/aaaa
+npm remove --no-save archiver change-case decompress-size directory-tree express-handlebars forever forever-monitor ip lodash monaco-editor network-interfaces node-fetch node-hid node-persist node-rest-client readdirp request serialport sharp simple-git smtp-server socket.io socket.io-client wedo xmljs
