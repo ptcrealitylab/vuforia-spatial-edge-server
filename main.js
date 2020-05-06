636d0b1dc4ee61b69b37e968079942d7c5f311a0
Anna Fuste <afuste@ptc.com>
2020-05-06 08:08:45 -0400

    Add Electron and Electron packager. Electron app in index.js at server

--- main.js
+++ main.js
@@ -1,49 +1,6 @@
-'use strict';
-
-const fork = require('child_process').fork;
-const path = require('path');
-const {app, BrowserWindow} = require('electron');
-
-const program = path.resolve('server.js');
-const parameters = [];
-const options = {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-};
-
-let child = null;
-
-function startNewChild() {
-    child = fork(program, parameters, options);
-    child.on('message', onChildMessage);
-    child.on('exit', onChildCrash);
-}
-
-function onChildMessage(message) {
-    if (message === 'restart') {
-        console.log('message from child:', message);
-        child.removeListener('exit', onChildCrash);
-        child.kill();
-        startNewChild();
-    } else  if (message === 'exit') {
-        child.removeListener('exit', onChildCrash);
-        child.kill();
-    }
-}
-
-function onChildCrash() {
-    console.info('server.js has exited unexpectedly, restarting');
-    child = fork(program, parameters, options);
-}
-
-startNewChild();
-
-
 // Modules to control application life and create native browser window
-
-
-console.log('******* APP: ', app, BrowserWindow);
-
-process.env.NODE_ENV = 'production';
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 function createWindow () {
     // Create the browser window.
@@ -79,3 +36,6 @@ app.on('activate', function () {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
