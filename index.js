7406dcd13cccc6fc33bb35e4baa7b9d5d316cc21
James Hobin <jhobin@ptc.com>
2020-05-11 11:07:00 -0400

    Allow 'node index.js' to work without weird errors

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ startNewChild();
 
 console.log('******* APP: ', app, BrowserWindow);
 
-function createWindow () {
+function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 900,
@@ -59,25 +59,33 @@ function createWindow () {
     // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+if (app) {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.whenReady().then(createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit();
-});
+    // Quit when all windows are closed.
+    app.on('window-all-closed', function() {
+        // On macOS it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
 
-app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 
-app.on('quit', function() {
-    child.removeListener('exit', onChildCrash);
-    child.kill();
-});
+    app.on('quit', function() {
+        child.removeListener('exit', onChildCrash);
+        child.kill();
+    });
+} else {
+    console.log('Electron unavailable on this platform');
+}
