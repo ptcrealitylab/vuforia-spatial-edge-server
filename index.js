cae80dda2399f2e39dfede0a1c02a92053149a77
James Hobin <jhobin@ptc.com>
2020-05-11 10:58:20 -0400

    Load server page

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function createWindow () {
     })
 
     // and load the index.html of the app.
-    mainWindow.loadFile('libraries/webInterface/gui/index.html')
+    mainWindow.loadURL('http://localhost:8080');
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
