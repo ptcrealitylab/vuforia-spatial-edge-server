de2ed39106bde02f6704f24d36b177df00a459b4
James Hobin <jhobin@ptc.com>
2020-08-27 10:50:42 -0400

    Improve mobile cleanup script

--- scripts/mobile-cleanup.sh
+++ scripts/mobile-cleanup.sh
@@ -4,4 +4,5 @@ set -ex
 # this would be good but prevents undoing this
 # rm -fr .git
 # rm -r hardwareInterfaces/*
-npm remove --no-save archiver change-case decompress-size directory-tree express-handlebars lodash monaco-editor network-interfaces node-persist readdirp sharp simple-git smtp-server xmljs
+npm prune --production
+npm uninstall --production --no-save archiver change-case decompress-size directory-tree express-handlebars lodash monaco-editor network-interfaces node-persist readdirp sharp simple-git smtp-server xmljs
