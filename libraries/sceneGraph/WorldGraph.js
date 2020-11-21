3b0c812553e93f90d25471028b35885f65a22b11
Ben Reynolds <breynolds@ptc.com>
2020-11-21 12:44:27 -0500

    implements WorldGraph to discover and add knownGraphs from other servers into one compiledGraph representing everything on the network

--- libraries/sceneGraph/WorldGraph.js
+++ libraries/sceneGraph/WorldGraph.js
@@ -1,22 +1,59 @@
 let SceneNode = require('./SceneNode');
 let SceneGraph = require('./SceneGraph');
 let utils = require('./utils');
+
 /**
- * The scene graph stores and computes the locations of all known objects, tools, and nodes.
- * It mirrors the information collected by Spatial Toolbox clients and can be used to perform
- * spatial computations on the server about the relative locations of various elements.
+ * The world graph has a localGraph, which is the sceneGraph of this server, and zero or more knownGraphs
+ * discovered from other servers, and is able to combine them into the compiledGraph
  */
 class WorldGraph {
-    constructor() {
-        this.localGraph = null;
+    constructor(localGraph) {
+        this.localGraph = localGraph;
         this.knownGraphs = {};
         this.compiledGraph = null;
+        this.updateCallbacks = [];
+    }
 
-        // // Add a root element to hold all objects, tools, nodes, etc
+    compile() {
+        // first add each part of the localGraph into the compiledGraph
+        this.compiledGraph = new SceneGraph();
+
+        // Add a root element to hold all objects, tools, nodes, etc
         // this.rootNode = new SceneNode(this.NAMES.ROOT);
         // this.graph[this.NAMES.ROOT] = this.rootNode;
 
-        this.updateCallbacks = [];
+        let localGraphCopy = this.localGraph.getSerializableCopy();
+        console.log('localGraph:');
+        // console.log(localGraphCopy);
+
+        this.compiledGraph.addDataFromSerializableGraph(localGraphCopy);
+
+        // then add each part of each knownGraph into the compiledGraph
+        for (let ip in this.knownGraphs) {
+            let thatGraph = this.knownGraphs[ip];
+            let knownGraphCopy = thatGraph.getSerializableCopy();
+
+            console.log('knownGraph (' + ip + '):');
+            // console.log(knownGraphCopy);
+
+            this.compiledGraph.addDataFromSerializableGraph(knownGraphCopy);
+        }
+
+        console.log(this.compiledGraph);
+        console.log('finished compiling graphs');
+
+        return this.compiledGraph;
+    }
+
+    addKnownGraph(graphId, knownGraphData) {
+        console.log('need to convert knownGraphData into a sceneGraph');
+        // maybe add an optional constructor param to SceneGraph that allows it to init from serializedCopy
+        let knownGraph = new SceneGraph();
+        knownGraph.addDataFromSerializableGraph(knownGraphData);
+
+        this.knownGraphs[graphId] = knownGraph;
+
+        this.compile();
     }
 
     onUpdate(callback) {
