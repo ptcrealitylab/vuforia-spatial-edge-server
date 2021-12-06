48313eefd87bee628fc8ae6efdd2aa351f868a38
James Hobin <jhobin@ptc.com>
2021-12-06 16:10:30 -0500

    Run eslint fix

--- libraries/objectDefaultFiles/gl-worker.js
+++ libraries/objectDefaultFiles/gl-worker.js
@@ -44,220 +44,221 @@ let realGl;
  * @return {any} a placeholder object from the local hidden gl context (realGl).
  */
 function makeStub(functionName) {
-  return function() {
-    const invokeId = id;
-    id += 1 + Math.random();
+    return function() {
+        const invokeId = id;
+        id += 1 + Math.random();
 
-    let args = Array.from(arguments);
-    for (let i = 0; i < args.length; i++) {
-      if (!args[i]) {
-        continue;
-      }
-      if (args[i].hasOwnProperty('__uncloneableId')) {
-        args[i] = {
-          fakeClone: true,
-          index: args[i].__uncloneableId,
-        };
-      } else if (typeof args[i] === 'object') {
-        if (args[i] instanceof Float32Array) {
-          args[i] = new Float32Array(args[i]);
-        } else if (args[i] instanceof Uint8Array) {
-          args[i] = new Uint8Array(args[i]);
-        } else if (args[i] instanceof Uint16Array) {
-          args[i] = new Uint16Array(args[i]);
-        } else if (args[i] instanceof Array) {
-          args[i] = Array.from(args[i]);
-        } else {
-          if (debugGlWorker) console.log('Uncloned arg', args[i]);
+        let args = Array.from(arguments);
+        for (let i = 0; i < args.length; i++) {
+            if (!args[i]) {
+                continue;
+            }
+            if (args[i].hasOwnProperty('__uncloneableId')) {
+                args[i] = {
+                    fakeClone: true,
+                    index: args[i].__uncloneableId,
+                };
+            } else if (typeof args[i] === 'object') {
+                if (args[i] instanceof Float32Array) {
+                    args[i] = new Float32Array(args[i]);
+                } else if (args[i] instanceof Uint8Array) {
+                    args[i] = new Uint8Array(args[i]);
+                } else if (args[i] instanceof Uint16Array) {
+                    args[i] = new Uint16Array(args[i]);
+                } else if (args[i] instanceof Array) {
+                    args[i] = Array.from(args[i]);
+                } else {
+                    if (debugGlWorker) console.log('Uncloned arg', args[i]);
+                }
+            }
         }
-      }
-    }
 
-    if (functionName === 'texImage2D') {
-      let elt = args[args.length - 1];
-      if (elt.tagName === 'IMG') {
-        let width = elt.width;
-        let height = elt.height;
-        let canvas = document.createElement('canvas');
-        canvas.width = width;
-        canvas.height = height;
-        let gfx = canvas.getContext('2d');
-        gfx.width = width;
-        gfx.height = height;
-        gfx.drawImage(elt, 0, 0, width, height);
-        let imageData = gfx.getImageData(0, 0, width, height);
-        args[args.length - 1] = imageData;
-      } else if (elt.tagName === 'CANVAS' ||
+        if (functionName === 'texImage2D') {
+            let elt = args[args.length - 1];
+            if (elt.tagName === 'IMG') {
+                let width = elt.width;
+                let height = elt.height;
+                let canvas = document.createElement('canvas');
+                canvas.width = width;
+                canvas.height = height;
+                let gfx = canvas.getContext('2d');
+                gfx.width = width;
+                gfx.height = height;
+                gfx.drawImage(elt, 0, 0, width, height);
+                let imageData = gfx.getImageData(0, 0, width, height);
+                args[args.length - 1] = imageData;
+            } else if (elt.tagName === 'CANVAS' ||
                  (typeof OffscreenCanvas !== 'undefined' && elt instanceof OffscreenCanvas)) {
-        let width = elt.width;
-        let height = elt.height;
-        let gfx = elt.getContext('2d');
-        let imageData = gfx.getImageData(0, 0, width, height);
-        args[args.length - 1] = imageData;
-      }
-    }
+                let width = elt.width;
+                let height = elt.height;
+                let gfx = elt.getContext('2d');
+                let imageData = gfx.getImageData(0, 0, width, height);
+                args[args.length - 1] = imageData;
+            }
+        }
 
-    if (functionName === 'getExtension') {
-      const ext = arguments[0];
-      // Blacklist unproxied extensions
-      if (ext === 'OES_vertex_array_object' ||
+        if (functionName === 'getExtension') {
+            const ext = arguments[0];
+            // Blacklist unproxied extensions
+            if (ext === 'OES_vertex_array_object' ||
           // ext === 'EXT_frag_depth' ||
           // ext === 'EXT_shader_texture_lod' ||
           ext === 'EXT_blend_minmax') {
-        return null;
-      }
-    }
-
-    const message = {
-      workerId,
-      id: invokeId,
-      name: functionName,
-      args,
-    };
-
-    frameCommandBuffer.push(message);
-
-    if (realGl) {
-      const unclonedArgs = Array.from(arguments).map(a => {
-        if (!a) {
-          return a;
-        }
-
-        if (a.__uncloneableId && !a.__uncloneableObj) {
-          console.error('invariant ruined');
-        }
-
-        if (a.__uncloneableObj) {
-          return a.__uncloneableObj;
-        }
-        return a;
-      });
-
-      const res = realGl[functionName].apply(realGl, unclonedArgs);
-
-      if (typeof res === 'object') {
-        let proxy = new Proxy({
-          __uncloneableId: invokeId,
-          __uncloneableObj: res,
-        }, {
-          get: function(obj, prop) {
-            if (prop === 'hasOwnProperty' || prop.startsWith('__')) {
-              return obj[prop];
-            } else {
-              // TODO this won't propagate to container
-              const mocked = obj.__uncloneableObj[prop];
-              if (typeof mocked === 'function') {
-                console.error('Unmockable inner function', prop);
-                return mocked.bind(obj.__uncloneableObj);
-              }
-              return mocked;
+                return null;
             }
-          },
-        });
+        }
 
-        proxies.push(proxy);
-        return proxy;
-      }
-      return res;
-    }
+        const message = {
+            workerId,
+            id: invokeId,
+            name: functionName,
+            args,
+        };
 
-    // if (functionName === 'getParameter') {
-    //   return cacheGetParameter[arguments[0]];
-    // }
+        frameCommandBuffer.push(message);
 
-    if (wantsResponse) {
-      return new Promise(res => {
-        pending[invokeId] = res;
-      });
-    }
-  };
+        if (realGl) {
+            const unclonedArgs = Array.from(arguments).map(a => {
+                if (!a) {
+                    return a;
+                }
+
+                if (a.__uncloneableId && !a.__uncloneableObj) {
+                    console.error('invariant ruined');
+                }
+
+                if (a.__uncloneableObj) {
+                    return a.__uncloneableObj;
+                }
+                return a;
+            });
+
+            const res = realGl[functionName].apply(realGl, unclonedArgs);
+
+            if (typeof res === 'object') {
+                let proxy = new Proxy({
+                    __uncloneableId: invokeId,
+                    __uncloneableObj: res,
+                }, {
+                    get: function(obj, prop) {
+                        if (prop === 'hasOwnProperty' || prop.startsWith('__')) {
+                            return obj[prop];
+                        } else {
+                            // TODO this won't propagate to container
+                            const mocked = obj.__uncloneableObj[prop];
+                            if (typeof mocked === 'function') {
+                                console.error('Unmockable inner function', prop);
+                                return mocked.bind(obj.__uncloneableObj);
+                            }
+                            return mocked;
+                        }
+                    },
+                });
+
+                proxies.push(proxy);
+                return proxy;
+            }
+            return res;
+        }
+
+        // if (functionName === 'getParameter') {
+        //   return cacheGetParameter[arguments[0]];
+        // }
+
+        if (wantsResponse) {
+            return new Promise(res => {
+                pending[invokeId] = res;
+            });
+        }
+    };
 }
 
 window.addEventListener('message', function(event) {
-  const message = event.data;
-  if (!message) {
-    if (debugGlWorker) console.warn('Event missing data', message);
-    return;
-  }
-  if (typeof message !== 'object') {
-      return;
-  }
-
-  if (message.name === 'bootstrap') {
-    for (const fnName of message.functions) {
-      gl[fnName] = makeStub(fnName);
+    const message = event.data;
+    if (!message) {
+        if (debugGlWorker) console.warn('Event missing data', message);
+        return;
+    }
+    if (typeof message !== 'object') {
+        return;
     }
 
-    gl = new Proxy(gl, {
-      get: function(obj, prop) {
-        // TODO dynamically stub
-        // if (typeof obj[prop] === 'function') {
-        // }
-        return obj[prop];
-      },
-    });
+    if (message.name === 'bootstrap') {
+        for (const fnName of message.functions) {
+            gl[fnName] = makeStub(fnName);
+        }
+
+        gl = new Proxy(gl, {
+            get: function(obj, prop) {
+                // TODO dynamically stub
+                // if (typeof obj[prop] === 'function') {
+                // }
+                return obj[prop];
+            },
+        });
 
 
-    for (const constName in message.constants) {
-      gl[constName] = message.constants[constName];
-    }
-    let {width, height} = message;
+        for (const constName in message.constants) {
+            gl[constName] = message.constants[constName];
+        }
+        let {width, height} = message;
 
-    frameCommandBuffer = [];
-    if (typeof main !== 'undefined') {
-        main({width, height});
-    }
+        frameCommandBuffer = [];
+        if (typeof main !== 'undefined') {
+            // eslint-disable-next-line no-undef
+            main({width, height});
+        }
 
-    if (window.glProxy.main) {
-        window.glProxy.main({width, height});
-    }
-    if (frameCommandBuffer.length > 0) {
-      window.parent.postMessage({
-        workerId,
-        messages: frameCommandBuffer,
-      }, '*');
-    }
-    return;
-  }
-
-  if (message.hasOwnProperty('id') && pending.hasOwnProperty(message.id)) {
-    pending[message.id](message.result);
-    delete pending[message.id];
-  }
-
-  if (message.name === 'frame') {
-    if (Date.now() - message.time > 300) {
-      console.log('time drift detected');
-      window.parent.postMessage({
-        workerId,
-        isFrameEnd: true,
-      }, '*');
-      return;
+        if (window.glProxy.main) {
+            window.glProxy.main({width, height});
+        }
+        if (frameCommandBuffer.length > 0) {
+            window.parent.postMessage({
+                workerId,
+                messages: frameCommandBuffer,
+            }, '*');
+        }
+        return;
     }
 
-    frameCommandBuffer = [];
-
-    if (render) {
-        render(message.time);
-    }
-    if (window.glProxy.render) {
-        window.glProxy.render(message.time);
+    if (message.hasOwnProperty('id') && pending.hasOwnProperty(message.id)) {
+        pending[message.id](message.result);
+        delete pending[message.id];
     }
 
-    if (frameCommandBuffer.length > 0) {
-      window.parent.postMessage({
-        workerId,
-        messages: frameCommandBuffer,
-      }, '*');
+    if (message.name === 'frame') {
+        if (Date.now() - message.time > 300) {
+            console.log('time drift detected');
+            window.parent.postMessage({
+                workerId,
+                isFrameEnd: true,
+            }, '*');
+            return;
+        }
+
+        frameCommandBuffer = [];
+
+        if (render) {
+            render(message.time);
+        }
+        if (window.glProxy.render) {
+            window.glProxy.render(message.time);
+        }
+
+        if (frameCommandBuffer.length > 0) {
+            window.parent.postMessage({
+                workerId,
+                messages: frameCommandBuffer,
+            }, '*');
+        }
+
+        frameCommandBuffer = [];
+
+        window.parent.postMessage({
+            workerId,
+            isFrameEnd: true,
+        }, '*');
     }
-
-    frameCommandBuffer = [];
-
-    window.parent.postMessage({
-      workerId,
-      isFrameEnd: true,
-    }, '*');
-  }
 });
 
 // eslint-disable-next-line no-unused-vars
