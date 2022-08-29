e67d63a8359cf06fbca35c80c9ebcc7cb34ace55
James Hobin <jhobin@ptc.com>
2022-08-29 10:58:02 -0400

    Update mobile-cleanup script

--- scripts/mobile-cleanup.sh
+++ scripts/mobile-cleanup.sh
@@ -4,5 +4,5 @@ set -ex
 # this would be good but prevents undoing this
 # rm -fr .git
 # rm -r hardwareInterfaces/*
-npm prune --production
-npm uninstall --production --no-save archiver change-case decompress-size directory-tree express-handlebars lodash monaco-editor network-interfaces node-persist readdirp sharp simple-git smtp-server xmljs
+npm ci --omit=dev
+npm uninstall --omit=dev --no-save archiver directory-tree monaco-editor node-persist readdirp simple-git
