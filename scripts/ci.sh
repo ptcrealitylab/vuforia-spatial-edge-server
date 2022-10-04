b38a3c453ea6d219f65f9033a2faef9a9f3bcac1
James Hobin <jhobin@ptc.com>
2022-10-04 13:22:33 -0400

    Add basic aio ci script

---
+++ scripts/ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm ci
+
+git clone https://github.com/hobinjk-ptc/test-spatialToolbox spatialToolbox
+
+cd addons
+
+# git clone https://github.com/ptcrealitylab/vuforia-spatial-edge-agent-addon
+git clone https://github.com/ptcrealitylab/vuforia-spatial-remote-operator-addon
+
+for i in `ls ./`; do echo $i; cd $i; npm ci; cd ..; done
+cd ..
