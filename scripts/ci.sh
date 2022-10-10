85e291f41e8ecdb3549c67292fc0c472481cd75a
James Hobin <jhobin@ptc.com>
2022-10-10 12:41:13 -0400

    Add back clone of edge-agent-addon

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,7 +9,7 @@ git clone https://github.com/hobinjk-ptc/test-spatialToolbox spatialToolbox
 
 cd addons
 
-# git clone https://github.com/ptcrealitylab/vuforia-spatial-edge-agent-addon
+git clone https://github.com/ptcrealitylab/vuforia-spatial-edge-agent-addon
 git clone https://github.com/ptcrealitylab/vuforia-spatial-remote-operator-addon
 
 for i in `ls ./`; do echo $i; cd $i; npm ci; cd ..; done
