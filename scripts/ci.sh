10d8eaf396146730a87376e5297c7a7dd2647514
James Hobin <55107443+hobinjk-ptc@users.noreply.github.com>
2022-10-10 16:46:37 -0400

    Integration test of server, remote operator, and edge agent (#607)
    * Add basic aio ci script
    
    * YAML problems
    
    * Okay then
    
    * Make script executable
    
    * Integrate puppeteer page load test
    
    * Allow saveToDisk
    
    * Install edge-agent
    
    * More useful hardware interface logging
    
    * Remove edge agent test
    
    * Increase test timeout
    
    * Kindly wait
    
    * Add screenshots during test process
    
    * Ignore gltf-added error
    
    * Disable all other tests
    
    * Add userinterface folder
    
    * Reduce timeout
    
    * Add screenshot of hardware interfaces
    
    * Ignore paths added during tests
    
    * Add back clone of edge-agent-addon
    
    * Add git configuration for private repo access
    
    * Add edge agent test
    
    * Add missing import
    
    * Re-enable older node versions
    
    * Create new version of build-quiet using latest work
    
    * Gracefully fail over to npm install
    
    * Switch older versions to simple smoketest
    
    * Only install prod dependencies for limited smoketest
    
    * Stop linting on older platforms

---
+++ scripts/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir screenshots
+
+npm ci || npm install
+
+git clone https://github.com/ptcrealitylab/vuforia-spatial-toolbox-userinterface
+git clone https://github.com/hobinjk-ptc/test-spatialToolbox spatialToolbox
+
+cd addons
+
+git clone https://github.com/ptcrealitylab/vuforia-spatial-edge-agent-addon
+git clone https://github.com/ptcrealitylab/vuforia-spatial-remote-operator-addon
+
+for i in `ls ./`; do echo $i; cd $i; npm ci; cd ..; done
+cd ..
--- test.js
+++ test.js
@@ -4,10 +4,84 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+const puppeteer = require('puppeteer');
+const fetch = require('node-fetch');
+
 // Start the server doing its own thing
 require('./server.js');
 
-setTimeout(() => {
-    console.log('The server is intact after 10 seconds');
+(async () => {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(
+        'http://localhost:8080/',
+        {
+            timeout: 60 * 1000,
+        },
+    );
+    await page.screenshot({
+        path: 'screenshots/server-settings.png',
+        fullPage: true,
+    });
+    await page.click('#manageHardwareInterfaces');
+    await page.screenshot({
+        path: 'screenshots/server-settings-hardware-interfaces.png',
+        fullPage: true,
+    });
+
+    await page.goto(
+        // `https://${localSettings.serverUrl}/stable/n/${localSettings.networkUUID}/s/${localSettings.networkSecret}/`,
+        'http://localhost:8081/',
+        {
+            timeout: 60 * 1000,
+        },
+    );
+    await page.screenshot({
+        path: 'screenshots/remote-operator-start-localhost.png',
+        fullPage: true,
+    });
+
+    await page.waitForSelector('#gltf-added', {
+        timeout: 60 * 1000
+    });
+
+    await page.screenshot({
+        path: 'screenshots/remote-operator-loaded-localhost.png',
+        fullPage: true,
+    });
+
+    const res = await fetch(`http://localhost:8080/hardwareInterface/edgeAgent/settings`);
+    const localSettings = await res.json();
+
+    await page.goto(
+        `https://${localSettings.serverUrl}/stable/n/${localSettings.networkUUID}/s/${localSettings.networkSecret}/`,
+        {
+            timeout: 60 * 1000,
+        },
+    );
+    await page.screenshot({
+        path: 'screenshots/remote-operator-start-proxied.png',
+        fullPage: true,
+    });
+
+    await page.waitForSelector('#gltf-added', {
+        timeout: 60 * 1000
+    });
+
+    await page.screenshot({
+        path: 'screenshots/remote-operator-loaded-proxied.png',
+        fullPage: true,
+    });
+
+
+    await page.close();
+
+    await browser.close();
+
+    console.log('basic remote operator through proxy server page load worked');
     process.exit(0);
-}, 10000);
+})();
