ba7de990fd0b5978272606280a8ecdd6bae96eb6
James Hobin <jhobin@ptc.com>
2022-10-18 15:45:39 -0400

    Improve mobile-cleanup script

--- scripts/mobile-cleanup.sh
+++ scripts/mobile-cleanup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ex
 
-# this would be good but prevents undoing this
-# rm -fr .git
-# rm -r hardwareInterfaces/*
 npm ci --omit=dev
-npm uninstall --omit=dev --no-save archiver directory-tree monaco-editor node-persist readdirp simple-git
+
+cd addons
+
+for i in `ls ./`; do echo $i; cd $i; npm ci --omit=dev; cd ..; done
+
+cd ..
+
+# npm uninstall --omit=dev --no-save archiver directory-tree monaco-editor node-persist readdirp simple-git
+
+npm uninstall --omit=dev --no-save monaco-editor
