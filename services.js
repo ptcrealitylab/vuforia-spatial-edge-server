cd90b05ca6abd3c5b7e61805cc9179ad775f4c74
benptc <breynolds@ptc.com>
2022-11-02 17:36:57 -0400

    update to remove yargs for now

--- services.js
+++ services.js
@@ -6,6 +6,19 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+// scrappy way to parse command line arguments until we drop support for Node.js 10.x
+let arguments = process.argv.slice(2); // this removes `node` and the filename from arguments list
+let args = {}
+if (arguments.indexOf("--services") !== -1) { // if it exists
+    let after = arguments.slice(arguments.indexOf("--services")+1);
+    let until = after.findIndex(value => value.indexOf('--') === 0); // find next
+    args.services = after.slice(0, (until === -1 ? undefined : until));
+} else {
+    args.services = [];
+}
+
+// Note: With minimum support for Node 12.x, we can use yargs instead:
+/*
 let yargs = require('yargs');
 
 let args = yargs
@@ -16,6 +29,9 @@ let args = yargs
         demand: false,
         default: []
     }).argv;
+
+console.log(args.services);
+*/
 
 module.exports.providedServices = args.services;
 module.exports.hasWorldService = args.services.includes('world');
