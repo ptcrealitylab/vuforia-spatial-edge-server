c2896bb0cf80f5df6dc75c3cb60163f3dd5fd466
James Hobin <jhobin@ptc.com>
2023-01-23 14:23:52 -0500

    Add periodic checkpointing to recorder; Add recorder compression

--- routers/history.js
+++ routers/history.js
@@ -1,12 +1,47 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+
+const recorder = require('../libraries/recorder.js');
+
 const router = express.Router();
-// const recorder = require('../libraries/recorder.js');
+
+const gzipStream = zlib.createGzip();
 
 let patches = [];
 
-router.get('/', function(req, res) {
+router.get('/logs', function(req, res) {
+    fs.readdir(recorder.logsPath, function (err, files) {
+        const logNames = {};
+        for (let file of files) {
+            if (file.endsWith('.json.gz')) {
+                let jsonLogName = file.split('.')[0] + '.json';
+                logNames[jsonLogName] = true;
+            }
+        }
+        res.json(Object.keys(logNames));
+    });
+});
+
+router.get('/logs/:logPath', function(req, res) {
     // res.json(recorder.timeObject);
-    res.sendFile('/Users/james/re/vuforia-spatial-edge-server/libraries/objectLogs/objects_1670865168825.json');
+    let compressedLogPath = path.join(recorder.logsPath, req.params.logPath + '.gz');
+    if (!fs.existsSync(compressedLogPath)) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.set('Vary', 'Accept-Encoding');
+    res.set('Content-Type', 'application/json');
+    if (req.get('Accept-Encoding').includes('gzip')) {
+        res.set('Content-Encoding', 'gzip');
+        const readStream = fs.createReadStream(compressedLogPath);
+        readStream.pipe(res);
+    } else {
+        const readStream = fs.createReadStream(compressedLogPath);
+        readStream.pipe(gzipStream).pipe(res);
+    }
 });
 
 router.get('/patches', function(req, res) {
