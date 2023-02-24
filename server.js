c1fd1e823abb1e000dbad72b71a91255acdfba58
Martin Klaudiny <mklaudiny@ptc.com>
2023-02-24 11:10:51 +0100

    Improved speed of sending heartbeat of new objects from server to listeners.

--- server.js
+++ server.js
@@ -1182,15 +1182,20 @@ function serverBeatSender(udpPort, oneTimeOnly = true) {
  * @param {string} thisVersion The version of the Object
  * @param {string} thisTcs The target checksum of the Object.
  * @param {boolean} oneTimeOnly if true the beat will only be sent once.
+ * @param {boolean} immediate if true the firdt beat will be sent immediately, not after beatInterval (works for one-time and periodic beats)
  **/
 
-function objectBeatSender(PORT, thisId, thisIp, oneTimeOnly) {
+function objectBeatSender(PORT, thisId, thisIp, oneTimeOnly, immediate) {
     if (isLightweightMobile) {
         return;
     }
 
     if (typeof oneTimeOnly === 'undefined') {
         oneTimeOnly = false;
+    }
+
+    if (typeof immediate === 'undefined') {
+        immediate = false;
     }
 
     if (!oneTimeOnly && activeHeartbeats[thisId]) {
@@ -1254,7 +1259,8 @@ function objectBeatSender(PORT, thisId, thisIp, oneTimeOnly) {
     });
 
     if (!oneTimeOnly) {
-        activeHeartbeats[thisId] = setInterval(function () {
+
+        function sendBeat() {
             // send the beat#
             if (thisId in objects && !objects[thisId].deactivated) {
                 // console.log("Sending beats... Content: " + JSON.stringify({ id: thisId, ip: thisIp, vn:thisVersionNumber, tcs: objects[thisId].tcs}));
@@ -1287,10 +1293,20 @@ function objectBeatSender(PORT, thisId, thisIp, oneTimeOnly) {
                     });
                 }
             }
-        }, beatInterval + utilities.randomIntInc(-250, 250));
-    } else {
+        };
+
+        // send one beat immediately and then start interval timer triggering beats
+        if (immediate) {
+            sendBeat();
+        }
+        // perturb the inverval a bit so that not all objects send the beat in the same time.
+        activeHeartbeats[thisId] = setInterval(sendBeat, beatInterval + utilities.randomIntInc(-250, 250));
+    } 
+    else {
         // Single-shot, one-time heartbeat
         // delay the signal with timeout so that not all objects send the beat in the same time.
+        let interval = immediate ? 0 : utilities.randomIntInc(1, 250);
+
         setTimeout(function () {
             // send the beat
             if (thisId in objects && !objects[thisId].deactivated) {
@@ -1316,7 +1332,7 @@ function objectBeatSender(PORT, thisId, thisIp, oneTimeOnly) {
                     client.close();
                 });
             }
-        }, utilities.randomIntInc(1, 250));
+        }, interval);
     }
 }
 exports.objectBeatSender = objectBeatSender;
@@ -2555,7 +2571,8 @@ function objectWebServer() {
 
                         sceneGraph.addObjectAndChildren(objectId, objects[objectId]);
 
-                        objectBeatSender(beatPort, objectId, objects[objectId].ip);
+                        // send the first beat immediately, so that there is a fast transmission of new object to all listeners
+                        objectBeatSender(beatPort, objectId, objects[objectId].ip, false, true);
 
                         var sendObject = {
                             id: objectId,
