663290c5f50a89e420b44f3ec401e452ff05c54c
Rik de Leeuw <rdeleeuw@ptc.com>
2023-04-05 17:04:24 +0200

    aframe and babylon widgets

---
+++ libraries/objectDefaultFiles/AFrameInterface.js
@@ -0,0 +1,137 @@
+/**
+ * @typedef {import("./object.js").SpatialInterface} SpatialInterface
+ */
+
+/**
+ * this interface mediates between the server, spatialinterface and the webworker
+ */
+class AFrameInterface {
+    /**
+     * 
+     * @param {SpatialInterface} spatialInterface
+     * @param {Worker} worker
+     */
+    constructor(spatialInterface, worker) {
+        // some information will become available after the bootstrap message has been received
+        this.spatialInterface = spatialInterface;
+        this.worker = worker;
+        this.workerId = -1;
+        this.prefersAttachingToWorld = true;
+        this.spatialInterface.useWebGlWorker();
+        this.spatialInterface.onSpatialInterfaceLoaded(this.onSpatialInterfaceLoaded.bind(this));
+        this.synclock = null;
+        this.touchAnswerListener = null;
+        this.mouse = {x: 0, y: 0};
+        this.lastTouchResult = false;
+    }
+
+    /**
+     * this message is used to setup the projection matrix in the webworker
+     * @param {Float32Array} modelViewMatrix 
+     * @param {Float32Array} projectionMatrix 
+     */
+    anchoredModelViewCallback(modelViewMatrix, projectionMatrix) {
+        this.worker.postMessage({name: "anchoredModelViewCallback", projectionMatrix: projectionMatrix});
+    }
+
+    /**
+     * after loading the spatial interface the worker can finish it's configuration
+     */
+    onSpatialInterfaceLoaded() {
+        this.spatialInterface.subscribeToMatrix();
+        this.spatialInterface.setFullScreenOn();
+
+        if (this.prefersAttachingToWorld) {
+            this.spatialInterface.prefersAttachingToWorld();
+        }
+
+        this.spatialInterface.addAnchoredModelViewListener(this.anchoredModelViewCallback.bind(this));
+
+        this.spatialInterface.setMoveDelay(300);
+        this.spatialInterface.registerTouchDecider(this.touchDecider.bind(this));
+    }
+
+    /**
+     * receives messages from the webworker to send to the server
+     * @param {MessageEvent<any>} event 
+     */
+    onMessageFromWorker(event) {
+        const message = event.data;
+        if (message) {
+            if ((this.touchAnswerListener !== null) && (typeof message === 'object') && (message.name === "touchDeciderAnswer")) {
+                this.lastTouchResult = message.result;
+                this.touchAnswerListener(true);
+            } else {
+                self.parent.postMessage(message, "*");
+            }
+        }
+    }
+
+    /**
+     * receives messages from the server to send through to the webworker
+     * some messages influence the spatial interface and are intercepted before passing them on
+     * @param {MessageEvent<any>} event 
+     */
+    onMessageFromServer(event) {
+        const message = event.data;
+        if (message && (typeof message === 'object')) {
+            if (message.hasOwnProperty("name")) {
+                if (message.name === "bootstrap") {
+                    // finish initalisation of the client
+                    const {workerId, width, height} = message;
+                    this.workerId = workerId;
+                    this.synclock = message.synclock;
+                    this.spatialInterface.changeFrameSize(width, height);
+                }
+            }
+            if ((this.synclock !== null) && Atomics.load(this.synclock, 0) === 0) {
+                // the webworker is in a locked state and unable to process incomming messages, drop the message and send an end frame message to make the server continue with the next proxy
+                console.log("worker " + this.workerId + " is waiting, dropping message");
+                if (message.hasOwnProperty("name") && (message.name === "frame")) {
+                    self.parent.postMessage({
+                        workerId: this.workerId,
+                        isFrameEnd: true,
+                    });
+                }
+            } else {
+                this.worker.postMessage(message);
+            }
+        }
+    }
+
+    /**
+     * 
+     * @returns {Promise<boolean>}
+     */
+    makeWatchdog() {
+        return new Promise((res) => {
+            setTimeout(res, 3000, false);
+        });
+    }
+
+    async touchDecider(eventData) {
+        //1. sets the mouse position with a coordinate system where the center
+        //   of the screen is the origin
+        this.mouse.x = (eventData.x / window.innerWidth) * 2 - 1;
+        this.mouse.y = -(eventData.y / window.innerHeight) * 2 + 1;
+
+        // if the webworker (containing the renderer) isn't sleeping, post touch message to analyse
+        if ((this.synclock !== null) && Atomics.load(this.synclock, 0) === 0) {
+             console.warn("tocuh decider locked worker, returning no touch");
+            return false;
+        }
+        this.worker.postMessage({name: "touchDecider", mouse: this.mouse, workerId: this.workerId});
+        let res = await Promise.race([this.makeWatchdog(), new Promise((result) => {
+            this.touchAnswerListener = result;
+        })]);
+        if (!res) {
+            console.warn("touch decider timeout, returning no touch");
+            this.touchAnswerListener = null;
+            return false;
+        }
+        this.touchAnswerListener = null;
+        return this.lastTouchResult;
+    }
+}
+
+export {AFrameInterface};
---
+++ libraries/objectDefaultFiles/AFrameWorker.js
@@ -0,0 +1,363 @@
+import {GLCommandBufferContext, CommandBufferFactory, CommandBuffer} from "/objectDefaultFiles/glCommandBuffer.js";
+import * as THREE from '/objectDefaultFiles/three/three.module.js';
+
+/**
+ * interfaces with three.js and intializes the engine for the rest of the tool to use for rendering
+ */
+class AFrameWorker {
+    static STATE_CONSTRUCTED = 0;
+    static STATE_BOOTSTRAP = 1;
+    static STATE_BOOTSTRAP_DONE = 2;
+    static STATE_FRAME = 3;
+    static STATE_FRAME_DONE = 4;
+    static STATE_CONTEXT_LOST = 5;
+    static STATE_CONTEXT_RESTORED = 6;
+    static STATE_CONTEXT_RESTORED_DONE = 7;
+    
+    constructor() {
+        this.clientState = AFrameWorker.STATE_CONSTRUCTED;
+        // some values will be set after the bootstrap message has been received
+        this.lastProjectionMatrix = null;
+        this.isProjectionMatrixSet = false;
+        this.workerId = -1;
+        this.synclock = null;
+        this.glCommandBufferContext = null;
+        this.commandBufferFactory = null;
+        this.frameCommandBuffer = null;
+        this.bootstrapProcessed = false;
+        /**
+         * @type {Array<function(THREE.Scene):void>}
+         */
+        this.onSceneCreatedCallbacks = [];
+        /**
+         * @type {Array<function(number):void>}
+         */
+        this.onRenderCallbacks = [];
+        this.raycaster = new THREE.Raycaster();
+    }
+
+    /**
+     * receives messages from the ThreeInteface client class
+     * @param {MessageEvent<any>} event 
+     */
+    onMessageFromInterface(event) {
+        const message = event.data;
+        if (!message) {
+            return;
+        }
+        if (typeof message !== 'object') {
+            return;
+        }
+        if (message.hasOwnProperty("name")) {
+            if (message.name === "touchDecider") {
+                let result = false;
+                if (this.clientState !== AFrameWorker.STATE_CONTEXT_LOST) {
+                    //2. set the picking ray from the camera position and mouse coordinates
+                    this.raycaster.setFromCamera(new THREE.Vector2(message.mouse.x, message.mouse.y), this.camera);
+
+                    //3. compute intersections
+                    const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+                    result = intersects.length > 0;
+                }
+                self.postMessage({workerId: this.workerId, name: "touchDeciderAnswer", result: result});
+            } else if (message.name === "anchoredModelViewCallback") {
+                switch (this.clientState) {
+                    case AFrameWorker.STATE_CONSTRUCTED:
+                    case AFrameWorker.STATE_BOOTSTRAP_DONE:
+                    case AFrameWorker.STATE_FRAME_DONE:
+                    case AFrameWorker.STATE_CONTEXT_RESTORED_DONE:
+                        // setup the projection matrix
+                        this.lastProjectionMatrix = message.projectionMatrix;
+                        break;
+                    default:
+                        console.error("wrong state to set projectionMatrix clientState: " + this.clientState);
+                        break;
+                }
+            } else if (message.name === "bootstrap") {
+                if (this.clientState == AFrameWorker.STATE_CONSTRUCTED) {
+                    this.clientState = AFrameWorker.STATE_BOOTSTRAP;
+                    // finish initialisation
+                    const {workerId, width, height, synclock} = message;
+                    this.workerId = workerId;
+                    this.synclock = synclock;
+    
+                    // create commandbuffer factory in order to create resource commandbuffers
+                    this.glCommandBufferContext = new GLCommandBufferContext(message);
+                    this.commandBufferFactory = new CommandBufferFactory(this.workerId, this.glCommandBufferContext, this.synclock);
+    
+                    // execute three.js startup
+                    const bootstrapCommandBuffers = this.main(width, height, this.commandBufferFactory);
+    
+                    // send all created commandbuffers
+                    for (const bootstrapCommandBuffer of bootstrapCommandBuffers) {
+                        bootstrapCommandBuffer.execute();
+                    }
+            
+                    // create standard rendering command buffer for reuse every frame
+                    this.frameCommandBuffer = this.commandBufferFactory.createAndActivate(true);
+    
+                    this.bootstrapProcessed = true;
+    
+                    // singnal end of frame
+                    self.postMessage({
+                        workerId: this.workerId,
+                        isFrameEnd: true,
+                    });
+                    this.clientState = AFrameWorker.STATE_BOOTSTRAP_DONE;
+                } else {
+                    console.error("wrong state for bootstrap clientsState: " + this.clientState);
+                }
+            } else if (message.name === "frame") {
+                switch (this.clientState) {
+                    case AFrameWorker.STATE_BOOTSTRAP_DONE:
+                    case AFrameWorker.STATE_FRAME_DONE:
+                    case AFrameWorker.STATE_CONTEXT_RESTORED_DONE:
+                        this.clientState = AFrameWorker.STATE_FRAME;
+                        // safety checks
+                        this.workerId = message.workerId;
+                        if (!this.bootstrapProcessed) {
+                            console.log(`Can't render worker with id: ${this.workerId}, it has not yet finished initializing`);
+                            self.postMessage({
+                                workerId: this.workerId,
+                                isFrameEnd: true,
+                            });
+                            this.clientState = AFrameWorker.STATE_FRAME_DONE;
+                            return;
+                        }
+                        if (Date.now() - message.time > 300) {
+                            console.log('time drift detected');
+                            self.postMessage({
+                                workerId: this.workerId,
+                                isFrameEnd: true,
+                            });
+                            this.clientState = AFrameWorker.STATE_FRAME_DONE;
+                            return;
+                        }
+        
+                        // erase the previous render command buffer
+                        this.frameCommandBuffer.clear();
+                        
+                        // try rendering with three.js
+                        try {
+                            this.frameCommandBuffer = this.render(message.time, this.frameCommandBuffer);
+                        } catch (err) {
+                            console.error('Error in gl-worker render fn', err);
+                        }
+        
+                        // send the commandbuffer (rendering or resource loading)
+                        this.frameCommandBuffer.execute();
+        
+                        // singnal end of frame
+                        self.postMessage({
+                            workerId: this.workerId,
+                            isFrameEnd: true,
+                        });
+                        this.clientState = AFrameWorker.STATE_FRAME_DONE;
+                        break;
+                    default:
+                        console.error("wrong state for generating frames clientState: " + this.clientState);
+                }
+            } else if (message.name === "context_lost") {
+                switch (this.clientState) {
+                    case AFrameWorker.CONSTRUCTED:
+                    case AFrameWorker.STATE_BOOTSTRAP_DONE:
+                    case AFrameWorker.STATE_FRAME_DONE:
+                    case AFrameWorker.STATE_CONTEXT_RESTORED_DONE:
+                        this.clientState = AFrameWorker.STATE_CONTEXT_LOST;
+                        this.onContextLost();
+                        break;
+                    default:
+                        console.error("wrong state for lost context clientState: " + this.clientState);
+                        break;
+                }
+            } else if (message.name === "context_restored") {
+                if (this.clientState === AFrameWorker.STATE_CONTEXT_LOST) {
+                    this.clientState = AFrameWorker.STATE_CONTEXT_RESTORED;
+                    this.onContextRestored();        
+                    this.clientState = AFrameWorker.STATE_CONTEXT_RESTORED_DONE;
+                } else {
+                    console.error("wrong state to restore state clientState: " + this.clientState);
+                }
+            }
+        }
+    }
+
+    onContextLost() {
+        this.glCommandBufferContext.onContextLost();
+        this.fakeCanvas.webglcontextlost({preventDefault: () => {}});
+    }
+
+    onContextRestored() {
+        this.glCommandBufferContext.onContextRestored();
+
+        let restoreBuffer = this.commandBufferFactory.createAndActivate(true);
+        this.fakeCanvas.webglcontextrestored();
+        restoreBuffer.execute();
+        
+        // singnal end of frame
+        self.postMessage({
+            workerId: this.workerId,
+            isFrameEnd: true,
+        });
+    }
+
+    /**
+     * registers callbacks for the tool to use during initalisation
+     * @param {function(THREE.Scene):void} callback 
+     * @returns {AFrameWorker}
+     */
+    onSceneCreated(callback) {
+        this.onSceneCreatedCallbacks.push(callback);
+        return this;
+    }
+
+    /**
+     * registers a callback for the tool to use during rendering
+     * @param {function(number):void} callback 
+     * @returns {AFrameWorker}
+     */
+    onRender(callback) {
+        this.onRenderCallbacks.push(callback);
+        return this;
+    }
+
+    /**
+     * starts three.js engine with the fake webgl context
+     * @param {number} width 
+     * @param {number} height 
+     * @param {CommandBufferFactory} cmdBufferFactory 
+     * @returns {CommandBuffer[]}
+     */
+    main(width, height, cmdBufferFactory) {
+        this.commandBufferFactory = cmdBufferFactory;
+        let cmdBuffer = cmdBufferFactory.createAndActivate(false);
+        let gl = cmdBufferFactory.getGL();
+        // this is needed to trick three.js into completing it's initialisation
+        this.fakeCanvas = new FakeCanvas(width, height);
+        this.renderer = new THREE.WebGLRenderer({context: gl, alpha: true, canvas: this.fakeCanvas});
+        this.renderer.debug.checkShaderErrors = false;
+        this.renderer.setSize(width, height);
+
+        // setup camera and scene for the tool
+        this.camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000);
+        this.scene = new THREE.Scene();
+
+        // call tool specific code to finish initialisation
+        for (let callback of this.onSceneCreatedCallbacks) {
+            callback(this.scene);
+        }
+        return [cmdBuffer];
+    }
+
+    /**
+     * renders the scene and returns the command buffer
+     * @param {number} now timestamp
+     * @param {CommandBuffer} commandBuffer the commandbuffer to use for rendering
+     * @returns CommandBuffer to send to the server
+     */
+    innerRender(now, commandBuffer) {
+        // safety checks
+        if (!this.camera) {
+            console.warn('rendering too early');
+            return commandBuffer;
+        }
+        if (!this.isProjectionMatrixSet && this.lastProjectionMatrix && this.lastProjectionMatrix.length === 16) {
+            // replace the projection matrix
+            setMatrixFromArray(this.camera.projectionMatrix, this.lastProjectionMatrix);
+            if (this.camera.projectionMatrixInverse.getInverse) {
+                this.camera.projectionMatrixInverse.getInverse(this.camera.projectionMatrix);
+            } else {
+                this.camera.projectionMatrixInverse.copy(this.camera.projectionMatrix).invert();
+            }
+            this.isProjectionMatrixSet = true;
+        }
+        // call tool specific code to finish the scene update cycle
+        for (let callback of this.onRenderCallbacks) {
+            callback(now);
+        }
+        // render the scene using three.js if posible
+        if (this.isProjectionMatrixSet) {
+            if (this.renderer && this.scene && this.camera) {
+                this.renderer.render(this.scene, this.camera);
+            }
+        }
+    }
+
+    /**
+     * tries to create a render command buffer. 
+     * during the first frame after adding assets the three.js renderer adds sync operations to initialize those new resources
+     * in that case the resulting bufer is not a render command buffer but a resource commandbuffer
+     * this code will detect that and send the resource command buffer to the server and rerender the whole scen to get a valid render commandbuffer to send to the server
+     * @param {number} now timestamp
+     * @param {CommandBuffer} commandBuffer the render commandbuffer to be used for rendering
+     * @returns {CommandBuffer} a render commandbuffer
+     */
+    render(now, commandBuffer) {
+        let threeCommandBuffer = this.commandBufferFactory.createAndActivate(false);
+
+        this.innerRender(now, threeCommandBuffer);
+
+        // check if three.js initialized resources during the renderloop
+        if (threeCommandBuffer.isCleared) {
+            // send remaining commands in the resource commandbuffer to the server
+            threeCommandBuffer.execute();
+            // rerender scene to get a renderable commandbuffer
+            this.glCommandBufferContext.setActiveCommandBuffer(commandBuffer);
+            this.innerRender(now, commandBuffer);
+        } else {
+            // three js created a valid render command buffer, replace the current buffer and enable rendering
+            commandBuffer = threeCommandBuffer;
+            commandBuffer.isRendering = true;
+        }
+
+        return commandBuffer;
+    }
+}
+
+/**
+ * Fake canvas element to make sure threejs initializes correctly
+ */
+class FakeCanvas {
+    /**
+     *
+     * @param {number} width
+     * @param {number} height
+     */
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.style = {width: width.toString() + "px", height: height.toString + "px"};
+        this.webglcontextlost = () => {};
+        this.webglcontextrestored = () => {};
+    }
+
+    /**
+     *
+     * @param {string} type
+     * @param {function(Event):void} listener
+     * @param {boolean} useCapture
+     */
+    addEventListener(type, listener, useCapture) {
+        // we don't implement context creation error
+        if (type === "webglcontextlost") {
+            this.webglcontextlost = listener;
+        } else if (type === "webglcontextrestored") {
+            this.webglcontextrestored = listener;
+        }
+    }
+}
+
+/**
+ * converts an Float32Array to a THREE.Matrix4
+ * @param {THREE.Matrix4} matrix
+ * @param {Float32Array} array
+ */
+function setMatrixFromArray(matrix, array) {
+    matrix.set(array[0], array[4], array[8], array[12],
+        array[1], array[5], array[9], array[13],
+        array[2], array[6], array[10], array[14],
+        array[3], array[7], array[11], array[15]
+    );
+}
+
+export {AFrameWorker, setMatrixFromArray};
