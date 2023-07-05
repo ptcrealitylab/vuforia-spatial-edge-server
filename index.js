c5a4d7c31fcd8fb4389bb5389a73a55b5c1d5ef9
James Hobin <jhobin@ptc.com>
2023-07-05 13:36:24 -0400

    Allow specifying spatialToolbox directory and udp broadcast port as arguments

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const fork = require('child_process').fork;
 const path = require('path');
+const argv = require('process').argv;
 
 const program = path.resolve('server.js');
-const parameters = [];
 const options = {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
 };
@@ -12,7 +12,7 @@ const options = {
 let child = null;
 
 function startNewChild() {
-    child = fork(program, parameters, options);
+    child = fork(program, argv, options);
     child.on('message', onChildMessage);
     child.on('exit', onChildCrash);
 }
@@ -31,7 +31,7 @@ function onChildMessage(message) {
 
 function onChildCrash() {
     console.info('server.js has exited unexpectedly, restarting');
-    child = fork(program, parameters, options);
+    child = fork(program, argv, options);
 }
 
 startNewChild();
