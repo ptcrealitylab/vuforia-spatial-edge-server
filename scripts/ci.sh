f3e1aae5e6788697706b15387f368bca1832d683
James Hobin <jhobin@ptc.com>
2023-09-25 16:00:33 -0400

    Try removing videoserver support

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -13,5 +13,12 @@ git clone https://github.com/ptcrealitylab/pop-up-onboarding-addon
 git clone https://github.com/ptcrealitylab/vuforia-spatial-edge-agent-addon
 git clone https://github.com/ptcrealitylab/vuforia-spatial-remote-operator-addon
 
-for i in `ls ./`; do echo $i; cd $i; npm ci; cd ..; done
+for i in `ls ./`; do
+  echo $i
+  cd $i
+  npm ci
+  npm uninstall --no-save @ffmpeg-installer/ffmpeg fsevents
+  cd ..
+done
+
 cd ..
