558cb6df96dd584af07e9a8f8718467eb097e1cd
James Hobin <jhobin@ptc.com>
2023-09-25 15:29:54 -0400

    Ignore userinterface tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 const config = {
     coveragePathIgnorePatterns: ["/node_modules/", "/addons/"],
     transformIgnorePatterns: ["/node_modules/", "/addons/", "\\.pnp\\.[^\\/]+$"],
+    testPathIgnorePatterns: ["vuforia-spatial-toolbox-userinterface"],
     moduleNameMapper: {
         "@libraries/(.*)$": "<rootDir>/libraries/$1",
     },
