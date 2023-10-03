133b994956abe361bd38851a49971b45cc472b7d
Rik de Leeuw <rdeleeuw@ptc.com>
2023-10-03 18:33:36 +0200

    fixing the desktop version

--- test.js
+++ test.js
@@ -35,7 +35,7 @@ require('./server.js');
 
     await page.goto(
         // `https://${localSettings.serverUrl}/stable/n/${localSettings.networkUUID}/s/${localSettings.networkSecret}/`,
-        'http://localhost:8080/userinterface',
+        'http://localhost:8081',
         {
             timeout: 60 * 1000,
         },
