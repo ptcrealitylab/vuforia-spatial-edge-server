9b53dedb02a1f85967fb0dd2f939fe024a07406a
James Hobin <jhobin@ptc.com>
2023-10-26 13:59:21 -0400

    Add synchronize-at-start experiment; Align mdb api more to fs

--- persistence/VerifyWrapper.js
+++ persistence/VerifyWrapper.js
@@ -1,10 +1,36 @@
 const mdb = require('./MongoDBWrapper.js');
 const fs = require('./FileSystemWrapper.js');
 
+const {synchronize} = require('./walkers.js');
+const {objectsPath} = require('../config.js');
+
+let syncInProgress = false;
+
+async function sync() {
+    if (syncInProgress) {
+        return;
+    }
+    syncInProgress = true;
+    try {
+        await synchronize(fs, mdb, objectsPath);
+    } catch (e) {
+        console.error('synchronize failed', e);
+    } finally {
+        syncInProgress = false;
+    }
+}
+
 const proxy = new Proxy({}, {
     get(target, prop) {
         return async function() {
-            console.log(prop, Array.from(arguments));
+            let debug = true;
+            if (prop === 'access') {
+                debug = false;
+            }
+            if (!arguments[0].includes('spatialToolbox')) {
+                debug = false;
+            }
+
             let mdbRes, mdbThrown;
             try {
                 mdbRes = await mdb[prop].apply(mdb, arguments);
@@ -17,18 +43,38 @@ const proxy = new Proxy({}, {
             } catch (e) {
                 fsThrown = e;
             }
+            if (fsThrown) {
+                if (debug) {
+                    console.log(prop, Array.from(arguments), fsThrown);
+                }
+                // Currently don't care about mdb's errors
+                throw fsThrown;
+            }
             if (mdbThrown && fsThrown) {
-                throw mdbThrown;
+                throw fsThrown;
             }
-            if ((!!mdbThrown) !== (!!fsThrown)) {
-                console.error('could not verify persistence', {mdbRes, fsRes, mdbThrown, fsThrown});
-                // process.exit(-1);
-                throw mdbThrown || fsThrown;
-            }
+            // if ((!!mdbThrown) !== (!!fsThrown)) {
+            //     console.error('could not verify persistence', {mdbRes, fsRes, mdbThrown, fsThrown});
+            //     // process.exit(-1);
+            //     throw mdbThrown || fsThrown;
+            // }
 
-            if (mdbRes !== fsRes) {
-                console.error('could not verify persistence', {mdbRes, fsRes});
+            let isBadStat = true;
+            if (prop === 'stat') {
+                if (mdbRes.isFile() === fsRes.isFile()) {
+                    isBadStat = false;
+                }
+            }
+            if (isBadStat && mdbRes !== fsRes) {
+                if (debug) {
+                    console.error(prop, Array.from(arguments), 'could not verify persistence', {mdbRes, fsRes});
+                }
+                sync();
                 // process.exit(-1);
+            } else {
+                if (debug) {
+                    // console.log(prop, Array.from(arguments), fsRes);
+                }
             }
             return fsRes;
         };
