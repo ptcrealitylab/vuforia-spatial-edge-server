51f1ed9fdba7172d2aae94f04b5733b2f3046166
Rik de Leeuw <rdeleeuw@ptc.com>
2024-02-05 17:15:42 -0500

    changes to certificate generation and reading them from the ios app

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # generate certificate and key
-libressl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -sha256 -days 356 -out cert.pem
+openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -sha256 -days 356 -out cert.pem
